262ffa75df3bc53a5978aa9e777819bf87e7cf41
Taehee <brad@Taeheeui-MacBookPro.local>
2020-09-09 13:26:26 +0900

    swap s3 and s5

--- session_05/main.py
+++ session_05/main.py
@@ -1,52 +1,43 @@
-from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
-from tensorflow.keras.models import load_model
-import numpy as np
 import cv2
+import numpy as np
 
-facenet = cv2.dnn.readNet('models/deploy.prototxt', 'models/res10_300x300_ssd_iter_140000.caffemodel')
-model = load_model('models/mask_detector.model')
+proto = 'models/colorization_deploy_v2.prototxt.txt'
+weights = 'models/colorization_release_v2.caffemodel'
 
-cap = cv2.VideoCapture('videos/04.mp4')
+net = cv2.dnn.readNetFromCaffe(proto, weights)
 
-while True:
-    ret, img = cap.read()
+pts_in_hull = np.load('models/pts_in_hull.npy')
+pts_in_hull = pts_in_hull.transpose().reshape(2, 313, 1, 1).astype(np.float32)
+net.getLayer(net.getLayerId('class8_ab')).blobs = [pts_in_hull]
 
-    if ret == False:
-        break
+net.getLayer(net.getLayerId('conv8_313_rh')).blobs = [np.full((1, 313), 2.606, np.float32)]
 
-    h, w, c = img.shape
+img = cv2.imread('imgs/01.jpg')
 
-    blob = cv2.dnn.blobFromImage(img, size=(300, 300), mean=(104., 177., 123.))
-    facenet.setInput(blob)
-    dets = facenet.forward()
+h, w, c = img.shape
 
-    for i in range(dets.shape[2]):
-        confidence = dets[0, 0, i, 2]
+img_input = img.copy()
 
-        if confidence < 0.5:
-            continue
+img_input = img_input.astype('float32') / 255.
+img_lab = cv2.cvtColor(img_input, cv2.COLOR_BGR2Lab)
+img_l = img_lab[:, :, 0:1]
 
-        x1 = int(dets[0, 0, i, 3] * w)
-        y1 = int(dets[0, 0, i, 4] * h)
-        x2 = int(dets[0, 0, i, 5] * w)
-        y2 = int(dets[0, 0, i, 6] * h)
+blob = cv2.dnn.blobFromImage(img_l, size=(224, 224), mean=[50, 50, 50])
 
-        face = img[y1:y2, x1:x2]
+net.setInput(blob)
+output = net.forward()
 
-        face_input = cv2.resize(face, dsize=(224, 224))
-        face_input = cv2.cvtColor(face_input, cv2.COLOR_BGR2RGB)
-        face_input = preprocess_input(face_input)
-        face_input = np.expand_dims(face_input, axis=0)
+output = output.squeeze().transpose((1, 2, 0))
 
-        mask, nomask = model.predict(face_input).squeeze()
+output_resized = cv2.resize(output, (w, h))
 
-        if mask > nomask:
-            color = (0, 255, 0)
-        else:
-            color = (0, 0, 255)
+output_lab = np.concatenate([img_l, output_resized], axis=2)
 
-        cv2.rectangle(img, pt1=(x1, y1), pt2=(x2, y2), thickness=2, color=color)
+output_bgr = cv2.cvtColor(output_lab, cv2.COLOR_Lab2BGR)
+output_bgr = output_bgr * 255
+output_bgr = np.clip(output_bgr, 0, 255)
+output_bgr = output_bgr.astype('uint8')
 
-    cv2.imshow('result', img)
-    if cv2.waitKey(1) == ord('q'):
-        break
+cv2.imshow('img', img_input)
+cv2.imshow('result', output_bgr)
+cv2.waitKey(0)
